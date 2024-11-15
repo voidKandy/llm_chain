8033a17dd129a83ac95a22f3bbb7e1d4c5c35653
voidKandy <voidkandy@gmail.com>
2024-11-15 10:09:13 -0800

    added identify

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ mod telemetry;
 use clap::Parser;
 use futures::StreamExt;
 use libp2p::{
-    floodsub::{Floodsub, Topic},
     gossipsub::{self, MessageAuthenticity},
+    identify,
     identity::Keypair,
-    kad::{self, store::MemoryStore, Mode, RecordKey},
-    swarm::{behaviour, NetworkBehaviour, SwarmEvent},
-    Multiaddr, PeerId,
+    kad::{self, store::MemoryStore, Mode, QueryId, QueryResult, RecordKey},
+    swarm::{NetworkBehaviour, SwarmEvent},
+    Multiaddr, PeerId, StreamProtocol, Swarm,
 };
 use serde::{Deserialize, Serialize};
 use std::{
     hash::{DefaultHasher, Hash, Hasher},
+    str::FromStr,
     sync::LazyLock,
     time::Duration,
 };
@@ -33,35 +34,59 @@ pub struct Args {
 static KEYS: LazyLock<Keypair> = LazyLock::new(|| Keypair::generate_ed25519());
 static PEER_ID: LazyLock<PeerId> = LazyLock::new(|| PeerId::from(KEYS.public()));
 
+// these should be mapped to real models down the line
+const MODEL_ID_0: &str = "model_0";
+const MODEL_ID_1: &str = "model_1";
 #[derive(Debug, Deserialize, Serialize)]
 enum Message<'m> {
     CompletionReq { model_id: u8, prompt: &'m str },
 }
 
 #[derive(NetworkBehaviour)]
-struct Behaviour {
+struct SysBehaviour {
     gossip: gossipsub::Behaviour,
     kad: kad::Behaviour<MemoryStore>,
+    identify: identify::Behaviour,
 }
 
-impl Behaviour {
+#[derive(Debug, Default)]
+struct ClientNodeData {
+    stdin: Option<Lines<BufReader<Stdin>>>,
+    model_prompt: Option<String>,
+    provider_query: Option<QueryId>,
+}
+
+const IDENTIFY_ID: &str = "/ipfs/id/1.0.0";
+impl SysBehaviour {
     fn new(peer_id: PeerId, key: Keypair) -> Self {
         let message_id_fn = |message: &gossipsub::Message| {
             let mut s = DefaultHasher::new();
             message.data.hash(&mut s);
             gossipsub::MessageId::from(s.finish().to_string())
         };
+        let identify =
+            identify::Behaviour::new(identify::Config::new(IDENTIFY_ID.to_string(), key.public()));
 
         let peer_store = MemoryStore::new(peer_id);
         let gossip_config = gossipsub::ConfigBuilder::default()
             .message_id_fn(message_id_fn)
             .build()
             .expect("failed to build gossip config");
+
         let gossip =
             gossipsub::Behaviour::new(MessageAuthenticity::Signed(key), gossip_config).unwrap();
-        Behaviour {
+
+        let mut kad_config = kad::Config::new(StreamProtocol::new("/idontknowhwhattocallthis"));
+
+        // Good for debugging, by default this is set to 5 mins
+        // kad_config.set_periodic_bootstrap_interval(Some(Duration::from_secs(10)));
+
+        let kad = kad::Behaviour::<MemoryStore>::with_config(peer_id, peer_store, kad_config);
+
+        SysBehaviour {
             gossip,
-            kad: kad::Behaviour::<MemoryStore>::new(peer_id, peer_store),
+            kad,
+            identify,
         }
     }
 }
@@ -73,12 +98,74 @@ async fn read_stdin_opt(stdinopt: &mut Option<Lines<BufReader<Stdin>>>) -> Optio
     None
 }
 
+async fn handle_swarm_event(
+    swarm: &mut Swarm<SysBehaviour>,
+    client_node_data: &mut ClientNodeData,
+    event: SwarmEvent<SysBehaviourEvent>,
+) {
+    match event {
+        SwarmEvent::NewListenAddr { address, .. } => warn!("Listening on {address:?}"),
+        SwarmEvent::Behaviour(beh_event) => {
+            match beh_event {
+                SysBehaviourEvent::Gossip(gossipsub::Event::Message {
+                    propagation_source,
+                    message_id,
+                    message,
+                }) => {
+                    let deserialized: Message =
+                        serde_json::from_slice(&message.data).expect("failed to deserialize");
+
+                    warn!("Got message: '{deserialized:#?}' with id: {message_id} from peer: {propagation_source}");
+                }
+                SysBehaviourEvent::Kad(kad_e) => match kad_e {
+                    kad::Event::OutboundQueryProgressed {
+                        id,
+                        result,
+                        stats,
+                        step,
+                    } => {
+                        // if client_node_data.provider_query == Some(id) {
+                        //     warn!("client node found a provider:\nresult={result:#?}\nstats={stats:#?}");
+                        // }
+                        match result {
+                            QueryResult::StartProviding(res) => {
+                                warn!("started providing: {res:#?}");
+                            }
+                            QueryResult::GetProviders(res) => {
+                                warn!("get providers result: {res:#?}");
+                            }
+                            _ => {}
+                        }
+                    }
+                    _ => {
+                        warn!("kad event: {kad_e:#?}");
+                    }
+                },
+                _ => {
+                    warn!("unhandled behavior event: {beh_event:#?}");
+                }
+            }
+        }
+        SwarmEvent::ConnectionEstablished { peer_id, .. } => {
+            warn!("New connection to peer: {peer_id:#?}")
+        }
+        SwarmEvent::ConnectionClosed { peer_id, .. } => {
+            warn!("Closed connection to peer: {peer_id:#?}")
+        }
+        event => {
+            tracing::error!("Unhandled Event: {event:#?}");
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> MainResult<()> {
     LazyLock::force(&TRACING);
     let args = Args::parse();
     warn!("args: {args:#?}");
     let local_id = LazyLock::force(&PEER_ID);
+
+    let mut client_node_data = ClientNodeData::default();
 
     warn!("Peer Id: {}", &local_id);
 
@@ -89,149 +176,55 @@ async fn main() -> MainResult<()> {
             libp2p::tls::Config::new,
             libp2p::yamux::Config::default,
         )?
-        .with_behaviour(|key| Behaviour::new(*local_id, key.clone()))?
+        .with_behaviour(|key| SysBehaviour::new(*local_id, key.clone()))?
         .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX)))
         .build();
 
     let topic = gossipsub::IdentTopic::new("test-net");
     swarm.behaviour_mut().gossip.subscribe(&topic)?;
-    // THIS IS HOW YOU DO HETEROGENEOUS
-    let mut stdinopt = None;
+    let key = RecordKey::new(&MODEL_ID_0);
+
     if args.server {
-        warn!("making this node a server");
         swarm.behaviour_mut().kad.set_mode(Some(Mode::Server));
 
-        let id = swarm
+        let qid = swarm
             .behaviour_mut()
             .kad
-            .start_providing(RecordKey::new(b"some_model"))
+            .start_providing(key.clone())
             .expect("failed to make node a provider");
+
+        warn!("making this node a server, qid: {qid:#?}");
     } else {
-        stdinopt = Some(tokio::io::BufReader::new(tokio::io::stdin()).lines());
+        client_node_data.stdin = Some(tokio::io::BufReader::new(tokio::io::stdin()).lines());
     }
 
-    // Tell the swarm to listen on all interfaces and a random, OS-assigned
-    // port.
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-
-    // Dial the peer identified by the multi-address given as the second
-    // command-line argument, if any.
     if let Some(addr) = args.dial_addr {
         let remote: Multiaddr = addr.parse()?;
         swarm.dial(remote)?;
         warn!("Dialed {addr}")
+    } else {
     }
 
     loop {
         tokio::select! {
-                Some(line) = read_stdin_opt(&mut stdinopt) => {
-                    let message = Message::CompletionReq{model_id: 0, prompt: &line};
-                    let json_byte_vec = serde_json::to_vec(&message).expect("failed serialization of message");
-                    if let Err(e) = swarm
-                        .behaviour_mut().gossip
-                        .publish(topic.clone(), json_byte_vec) {
-                        warn!("Publish error: {e:?}");
-                    }
-                }
+            Some(line) = read_stdin_opt(&mut client_node_data.stdin) => {
+                client_node_data.provider_query= Some(swarm
+                    .behaviour_mut()
+                    .kad
+                    .get_providers(key.clone()));
+                client_node_data.model_prompt = Some(line);
+                warn!("updated client node: {client_node_data:#?}");
 
-            event = swarm.select_next_some() => match event {
-
-            SwarmEvent::NewListenAddr { address, .. } => warn!("Listening on {address:?}"),
-            SwarmEvent::Behaviour(beh_event) => {
-                match beh_event {
-                    BehaviourEvent::Gossip(gossipsub::Event::Message {
-                        propagation_source,
-                        message_id,
-                        message
-                    }) => {
-                        let deserialized: Message = serde_json::from_slice(&message.data).expect("failed to deserialize");
-                        warn!("Got message: '{deserialized:#?}' with id: {message_id} from peer: {propagation_source}");
-                    },
-                    BehaviourEvent::Kad(kad_e) => { warn!("kad event: {kad_e:#?}"); },
-                    _ => {warn!("unhandled behavior event: {beh_event:#?}");}
-
-                }
-            },
-            SwarmEvent::ConnectionEstablished { peer_id, .. } => {
-                warn!("New connection to peer: {peer_id:#?}")
+                // let message = Message::CompletionReq{model_id: MODEL_ID_0, prompt: &line};
+                // let json_byte_vec = serde_json::to_vec(&message).expect("failed serialization of message");
+                // if let Err(e) = swarm
+                //     .behaviour_mut().gossip
+                //     .publish(topic.clone(), json_byte_vec) {
+                //     warn!("Publish error: {e:?}");
+                // }
             }
-            SwarmEvent::ConnectionClosed { peer_id, .. } => {
-                warn!("Closed connection to peer: {peer_id:#?}")
-            }
-            event => {
-                tracing::error!("Unhandled Event: {event:#?}");
-            }
-            //     SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
-            //         for (peer_id, _multiaddr) in list {
-            //             println!("mDNS discovered a new peer: {peer_id}");
-            //             swarm.behaviour_mut().gossip.add_explicit_peer(&peer_id);
-            //         }
-            //     },
-            //     SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
-            //         for (peer_id, _multiaddr) in list {
-            //             println!("mDNS discover peer has expired: {peer_id}");
-            //             swarm.behaviour_mut().gossip.remove_explicit_peer(&peer_id);
-            //         }
-            //     },
-            //     SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
-            //         propagation_source: peer_id,
-            //         message_id: id,
-            //         message,
-            //     })) => println!(
-            //             "Got message: '{}' with id: {id} from peer: {peer_id}",
-            //             String::from_utf8_lossy(&message.data),
-            //         ),
-            //     SwarmEvent::NewListenAddr { address, .. } => {
-            //         println!("Local node is listening on {address}");
-            //     }
-            //     _ => {}
-            }
+            event = swarm.select_next_some() => handle_swarm_event(&mut swarm, &mut client_node_data, event).await
         }
     }
-
-    // loop {
-    //     let evt = {
-    //         tokio::select! {
-    //             line = stdin.next_line() => Some(EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
-    //             event = swarm.next() => {
-    //                 info!("Unhandled Swarm Event: {:?}", event);
-    //                 None
-    //             },
-    //             response = response_rcv.recv() => Some(EventType::Response(response.expect("response exists"))),
-    //         }
-    //     };
-    //     ...
-    // }
-
-    // loop {
-    //     let evt = {
-    //         tokio::select! {
-    //             line = stdin.next_line() => Some(line.expect("can get line").expect("can read line from stdin")),
-    //             event = swarm.next() => {
-    //                 warn!("Unhandled Swarm Event: {:?}", event);
-    //                 None
-    //             },
-    //             // response = response_rcv.recv() => Some(EventType::Response(response.expect("response exists"))),
-    //         }
-    //     };
-    //
-    //     if let Some(txt) = evt {
-    //         warn!("Got from stdin: {txt}");
-    //     }
-    // }
-    //     // loop {
-    //     match swarm.select_next_some().await {
-    //         SwarmEvent::NewListenAddr { address, .. } => warn!("Listening on {address:?}"),
-    //         SwarmEvent::Behaviour(event) => warn!("EVENT: {event:?}"),
-    //         SwarmEvent::ConnectionEstablished { peer_id, .. } => {
-    //             warn!("New connection to peer: {peer_id:#?}")
-    //         }
-    //         SwarmEvent::ConnectionClosed { peer_id, .. } => {
-    //             warn!("Closed connection to peer: {peer_id:#?}")
-    //         }
-    //         event => {
-    //             tracing::error!("Unhandled Event: {event:#?}");
-    //         }
-    //     }
-    // }
 }
